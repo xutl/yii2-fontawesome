ada9647d81c8c6282f58d3dd2e17122d3ff90de2
xutl <79109681@qq.com>
2016-09-03 15:49:01 +0800

    init

---
+++ Asset.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\fontawesome;
+
+use yii\web\AssetBundle;
+
+class Asset extends AssetBundle
+{
+    /**
+     * @inherit
+     */
+    public $sourcePath = '@vendor/xutl/yii2-fontawesome/assets';
+
+    /**
+     * @inherit
+     */
+    public $css = [
+        'css/font-awesome.min.css',
+    ];
+
+    /**
+     * Initializes the bundle.
+     * Set publish options to copy only necessary files (in this case css and font folders)
+     * @codeCoverageIgnore
+     */
+    public function init()
+    {
+        parent::init();
+        $this->publishOptions['beforeCopy'] = function ($from, $to) {
+            return preg_match('%(/|\\\\)(fonts|css)%', $from);
+        };
+    }
+}
