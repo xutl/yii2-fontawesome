02a7052b7283a9b4d1faa395ebeff4b0ad841749
xutl <79109681@qq.com>
2016-09-03 17:33:32 +0800

    增加CDN地址

---
+++ CDNAsset.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\fontawesome;
+
+use yii\web\AssetBundle;
+
+class Asset extends AssetBundle
+{
+
+
+    /**
+     * @inherit
+     */
+    public $css = [
+        '//cdn.bootcss.com/font-awesome/4.6.3/css/font-awesome.min.css',
+    ];
+
+
+}
