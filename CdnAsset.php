a02eaf99f5a0ba95d155d93c9076fb60ab9fdbbe
xutl <79109681@qq.com>
2016-12-03 14:32:07 +0800

    规范Cdn资源

---
+++ CdnAsset.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\fontawesome;
+
+use yii\web\AssetBundle;
+
+class CdnAsset extends AssetBundle
+{
+    /**
+     * @inherit
+     */
+    public $css = [
+        '//lib.baomitu.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    ];
+
+
+}
